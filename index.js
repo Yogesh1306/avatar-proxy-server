966683ef48be921f5d6cb07dcaef8f4a767f8f90
Yogesh Joshi <140999818+Yogesh1306@users.noreply.github.com>
2026-04-05 17:15:38 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+import express from "express";
+import cors from "cors";
+import axios from "axios";
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(cors());
+
+app.get("/proxy", async (req, res) => {
+  const imageUrl = req.query.url;
+  if (!imageUrl) return res.status(400).send("Url is required");
+  try {
+    const response = await axios.get(imageUrl, { responseType: "arraybuffer" });
+    const contentType = response.headers["content-Type"];
+    res.setHeader("Content-Type", contentType);
+    res.send(response.data);
+  } catch (error) {
+    res.status(500).send("Error fetching image");
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Proxy server running on port ${PORT}`);
+});
